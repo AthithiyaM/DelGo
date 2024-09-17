8cc5c644167b13c4b15d6459d6ab2824276907df
AthithiyaM <athi4758@gmail.com>
2024-09-17 11:52:10 -0400

    Implemented CNNs! and some cleanup

--- code/extra_scripts/evaluating_with_known_position.py
+++ code/extra_scripts/evaluating_with_known_position.py
@@ -1,18 +1,18 @@
 # This script can be used to feed a board into the trained model and print out its predictions
 
 import numpy as np
-import loading_and_preprocessing
+from CNN import loading_and_preprocessing # This can be changed to any of the NNs
 
 test_board = np.array([[
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 1, -1,  1, -1, 0, 0, 0, 0,
-    0, 1, -1,  1, -1, 0, 0, 0, 0,
-    0, 0,  1, -1,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
+    [0, 1, -1,  1, -1, 0, 0, 0, 0,],
+    [0, 1, -1,  1, -1, 0, 0, 0, 0,],
+    [0, 0,  1, -1,  0, 0, 0, 0, 0,],
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
+    [0, 0,  0,  0,  0, 0, 0, 0, 0,],
 ]])
 
 move_prob = loading_and_preprocessing.model.predict(test_board)[0]
